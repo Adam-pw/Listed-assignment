3c4b8bb16ef5ca661652085d192b302d955950f6
Adam-pw <adampithewan@gmail.com>
2023-07-06 16:39:58 +0530

    feat: :hammer: env changed

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,9 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId:
-        "271358346932-t64rmr4boouo53t6s39i5gm8ma40to6h.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.clientId ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
 });
